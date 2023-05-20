96fdce48ec6a3d0da142501fa560ff2e307bec15
somrat sorkar <themefisher.dev@gmail.com>
2023-05-20 10:01:34 +0600

    update contentParser

--- src/lib/contentParser.ts
+++ src/lib/contentParser.ts
@@ -1,85 +1,84 @@
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
+
 const contentPath = "src/content";
 
-// get list page data, ex: _index.md
-export const getListPage = (filePath: string) => {
-  try {
-    const pageData = fs.readFileSync(path.join(contentPath, filePath), "utf-8");
-    const pageDataParsed = matter(pageData);
-    const content = pageDataParsed.content;
-    const frontmatter = pageDataParsed.data;
+// Helper function to read file content
+const readFile = (filePath:string) => {
+  return fs.readFileSync(filePath, "utf-8");
+};
 
-    return {
-      notFound: false,
-      frontmatter,
-      content,
-    };
-    
-  } catch (error) {
+// Helper function to parse frontmatter
+const parseFrontmatter = (frontmatter:any) => {
+  const frontmatterString = JSON.stringify(frontmatter);
+  return JSON.parse(frontmatterString);
+};
+
+// get list page data, ex: _index.md
+export const getListPage = (filePath:string) => {
+  const pageDataPath = path.join(contentPath, filePath);
+
+  if (!fs.existsSync(pageDataPath)) {
     return {
       notFound: true,
       frontmatter: {},
       content: "",
     };
   }
+
+  const pageData = readFile(pageDataPath);
+  const { content, data: frontmatter } = matter(pageData);
+
+  return {
+    notFound: false,
+    frontmatter: parseFrontmatter(frontmatter),
+    content,
+  };
 };
 
 // get all single pages, ex: blog/post.md
-export const getSinglePage = (folder: string) => {
-  try {
-    const filesPath = fs.readdirSync(path.join(contentPath, folder));
-    const sanitizeFiles = filesPath.filter((file) => file.endsWith(".md"));
-    const filterSingleFiles = sanitizeFiles.filter((file) =>
-      file.match(/^(?!_)/)
-    );
-    const singlePages = filterSingleFiles.map((filename) => {
-      const slug = filename.replace(".md", "");
-  
-      try {
-        const pageData = fs.readFileSync(
-          path.join(contentPath, folder, filename),
-          "utf-8"
-        );
-        const pageDataParsed = matter(pageData);
-        const frontmatterString = JSON.stringify(pageDataParsed.data);
-        const frontmatter = JSON.parse(frontmatterString);
-        const content = pageDataParsed.content;
-        const url = frontmatter.url ? frontmatter.url.replace("/", "") : slug;
-  
-        return {
-          notFound: false,
-          frontmatter: frontmatter,
-          slug: url,
-          content: content
-        };
-      } catch (error) {
-        
-        return {
-          notFound: true,
-          frontmatter: {},
-          slug: "",
-          content: ""
-        };
-      }
-    });
+export const getSinglePage = (folder:string) => {
+  const folderPath = path.join(contentPath, folder);
 
-    const publishedPages = singlePages.filter(
-      (page) => !page.frontmatter.draft && page
-    );
-    const filterByDate = publishedPages.filter(
-      (page) => new Date(page.frontmatter.date || new Date()) <= new Date()
-    );
-  
-    return filterByDate;
-    
-  } catch (error) {
-    return [{
-      notFound: true,
-      frontmatter: {},
-      slug: "",
-      content: ""
-    }];
+  if (!fs.existsSync(folderPath) || !fs.lstatSync(folderPath).isDirectory()) {
+    return [
+      {
+        notFound: true,
+        frontmatter: {},
+        slug: "",
+        content: "",
+      },
+    ];
   }
+
+  const filesPath = fs.readdirSync(folderPath);
+  const sanitizeFiles = filesPath.filter((file) => file.endsWith(".md"));
+  const filterSingleFiles = sanitizeFiles.filter((file) =>
+    file.match(/^(?!_)/)
+  );
+
+  const singlePages = filterSingleFiles.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const filePath = path.join(folderPath, filename);
+    const pageData = readFile(filePath);
+    const { content, data: frontmatter } = matter(pageData);
+    const url = frontmatter.url ? frontmatter.url.replace("/", "") : slug;
+
+    return {
+      notFound: false,
+      frontmatter: parseFrontmatter(frontmatter),
+      slug: url,
+      content,
+    };
+  });
+
+  const publishedPages = singlePages.filter(
+    (page) => !page.frontmatter.draft && page
+  );
+  const filterByDate = publishedPages.filter(
+    (page) => new Date(page.frontmatter.date || new Date()) <= new Date()
+  );
+
+  return filterByDate;
 };
