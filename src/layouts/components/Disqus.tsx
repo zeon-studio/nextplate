c23aec3a34c12b89de6d9e5f1537ae5b34bc730d
somrat sorkar <themefisher.dev@gmail.com>
2023-05-30 10:59:29 +0600

    update tsconfig

--- src/layouts/components/Disqus.tsx
+++ src/layouts/components/Disqus.tsx
@@ -4,7 +4,7 @@ import config from "@/config/config.json";
 import { DiscussionEmbed } from "disqus-react";
 import { useTheme } from "next-themes";
 
-const Disqus = ({ className }: { className?: string }): JSX.Element => {
+const Disqus = ({ className }: { className?: string }) => {
   const { disqus } = config;
   const { theme } = useTheme();
 
