2ea8bacf8ffec964dfd34842d547d80e1a426673
somrat sorkar <themefisher.dev@gmail.com>
2023-07-27 10:32:00 +0600

    update navigation

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -48,26 +48,27 @@ const Header = () => {
         <input id="nav-toggle" type="checkbox" className="hidden" />
         <label
           htmlFor="nav-toggle"
-          className="order-3 cursor-pointer text-dark dark:text-white lg:order-1"
+          className="order-3 cursor-pointer flex items-center lg:hidden text-dark dark:text-white lg:order-1"
         >
-          <span
+          <svg
             id="show-button"
-            className="flex items-center lg:order-1 lg:hidden"
+            className="h-6 fill-current block"
+            viewBox="0 0 20 20"
           >
-            <svg className="h-6 fill-current" viewBox="0 0 20 20">
-              <title>Menu Open</title>
-              <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z"></path>
-            </svg>
-          </span>
-          <span id="hide-button" className="hidden items-center lg:order-1">
-            <svg className="h-6 fill-current" viewBox="0 0 20 20">
-              <title>Menu Close</title>
-              <polygon
-                points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
-                transform="rotate(45 10 10)"
-              ></polygon>
-            </svg>
-          </span>
+            <title>Menu Open</title>
+            <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z"></path>
+          </svg>
+          <svg
+            id="hide-button"
+            className="h-6 fill-current hidden"
+            viewBox="0 0 20 20"
+          >
+            <title>Menu Close</title>
+            <polygon
+              points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+              transform="rotate(45 10 10)"
+            ></polygon>
+          </svg>
         </label>
         {/* /navbar toggler */}
 
