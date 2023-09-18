27860609bb3c2cd5f41623f646b7416048943df1
Hery Lopez <heryjlr@gmail.com>
2023-09-18 16:40:55 +0200

    feat: remove get started button and its configuration code

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -26,7 +26,7 @@ export interface INavigationLink {
 const Header = () => {
   // distructuring the main menu from menu object
   const { main }: { main: INavigationLink[] } = menu;
-  const { navigation_button, settings } = config;
+  const { settings } = config;
   // get current path
   const pathname = usePathname();
 
@@ -127,16 +127,6 @@ const Header = () => {
               )}
             </React.Fragment>
           ))}
-          {navigation_button.enable && (
-            <li className="mt-4 inline-block lg:hidden">
-              <Link
-                className="btn btn-outline-primary btn-sm"
-                href={navigation_button.link}
-              >
-                {navigation_button.label}
-              </Link>
-            </li>
-          )}
         </ul>
         <div className="order-1 ml-auto flex items-center md:order-2 lg:ml-0">
           {settings.search && (
@@ -149,14 +139,6 @@ const Header = () => {
             </Link>
           )}
           <ThemeSwitcher className="mr-5" />
-          {navigation_button.enable && (
-            <Link
-              className="btn btn-outline-primary btn-sm hidden lg:inline-block"
-              href={navigation_button.link}
-            >
-              {navigation_button.label}
-            </Link>
-          )}
         </div>
       </nav>
     </header>
