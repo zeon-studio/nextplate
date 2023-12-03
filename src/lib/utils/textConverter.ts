39a6ed08754ceb0c68dea38299894e1d9ac5344f
somrat sorkar <themefisher.dev@gmail.com>
2023-12-03 12:16:14 +0600

    update packages

--- src/lib/utils/textConverter.ts
+++ src/lib/utils/textConverter.ts
@@ -26,7 +26,7 @@ export const humanize = (content: string) => {
 
 // plainify
 export const plainify = (content: string) => {
-  const parseMarkdown = marked.parse(content);
+  const parseMarkdown: any = marked.parse(content);
   const filterBrackets = parseMarkdown.replace(/<\/?[^>]+(>|$)/gm, "");
   const filterSpaces = filterBrackets.replace(/[\r\n]\s*[\r\n]/gm, "");
   const stripHTML = htmlEntityDecoder(filterSpaces);
