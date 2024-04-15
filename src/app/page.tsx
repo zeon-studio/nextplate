3614bedda4213d1fa53c332fed9dbb29ed51a1fb
Ceiara Madrigal <ceiaram@ninthavenuefoods.com>
2024-04-15 10:15:11 -0700

    Render svg icons correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -209,9 +209,9 @@ const Home = () => {
                       >
                         <path
                           stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="m9 5 7 7-7 7"
                         />
                       </svg>
--- src/layouts/partials/Services.tsx
+++ src/layouts/partials/Services.tsx
@@ -90,9 +90,9 @@ const ServicesComponent = ({ data }: { data: PageData }) => {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="m9 5 7 7-7 7"
                             />
                           </svg>
