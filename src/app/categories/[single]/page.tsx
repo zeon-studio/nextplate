344f6aa580f29ea0af0702e12406ea508ba6bd29
Somrat <58769763+tfsomrat@users.noreply.github.com>
2024-04-17 11:46:33 +0600

    fixed post sorting on taxonomy pages

--- src/app/categories/[single]/page.tsx
+++ src/app/categories/[single]/page.tsx
@@ -2,6 +2,7 @@ import BlogCard from "@/components/BlogCard";
 import config from "@/config/config.json";
 import { getSinglePage } from "@/lib/contentParser";
 import { getTaxonomy } from "@/lib/taxonomyParser";
+import { sortByDate } from "@/lib/utils/sortFunctions";
 import taxonomyFilter from "@/lib/utils/taxonomyFilter";
 import { humanize } from "@/lib/utils/textConverter";
 import PageHeader from "@/partials/PageHeader";
@@ -28,6 +29,7 @@ export const generateStaticParams: StaticParams = () => {
 const CategorySingle = ({ params }: { params: { single: string } }) => {
   const posts: Post[] = getSinglePage(blog_folder);
   const filterByCategories = taxonomyFilter(posts, "categories", params.single);
+  const sortedPosts = sortByDate(filterByCategories);
 
   return (
     <>
@@ -36,7 +38,7 @@ const CategorySingle = ({ params }: { params: { single: string } }) => {
       <div className="section-sm pb-0">
         <div className="container">
           <div className="row">
-            {filterByCategories.map((post: Post, index: number) => (
+            {sortedPosts.map((post: Post, index: number) => (
               <div className="mb-14 md:col-6 lg:col-4" key={index}>
                 <BlogCard data={post} />
               </div>
--- src/app/tags/[single]/page.tsx
+++ src/app/tags/[single]/page.tsx
@@ -2,6 +2,7 @@ import BlogCard from "@/components/BlogCard";
 import config from "@/config/config.json";
 import { getSinglePage } from "@/lib/contentParser";
 import { getTaxonomy } from "@/lib/taxonomyParser";
+import { sortByDate } from "@/lib/utils/sortFunctions";
 import taxonomyFilter from "@/lib/utils/taxonomyFilter";
 import { humanize } from "@/lib/utils/textConverter";
 import PageHeader from "@/partials/PageHeader";
@@ -28,6 +29,7 @@ export const generateStaticParams: StaticParams = () => {
 const TagSingle = ({ params }: { params: { single: string } }) => {
   const posts: Post[] = getSinglePage(blog_folder);
   const filterByTags = taxonomyFilter(posts, "tags", params.single);
+  const sortedPosts = sortByDate(filterByTags);
 
   return (
     <>
@@ -36,7 +38,7 @@ const TagSingle = ({ params }: { params: { single: string } }) => {
       <div className="section-sm pb-0">
         <div className="container">
           <div className="row">
-            {filterByTags.map((post: Post, index: number) => (
+            {sortedPosts.map((post: Post, index: number) => (
               <div className="mb-14 md:col-6 lg:col-4" key={index}>
                 <BlogCard data={post} />
               </div>
