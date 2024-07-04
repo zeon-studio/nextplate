67e8416a7d3dc0b44f618f2904819b84a0047238
arybecks23 <arybecks23@gmail.com>
2024-07-03 17:55:40 -0700

    Update next.config.js
    Change nextjs output

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
-  output: "standalone",
+  // output: "standalone",
   env: {
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
     SANITY_DATASET: process.env.SANITY_DATASET,
