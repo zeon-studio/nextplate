b3de29af2e2e7a3c377c9f9c61b603f343cff237
Ceiara Madrigal <ceiaram@ninthavenuefoods.com>
2024-07-08 15:28:56 -0700

    Update

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -19,12 +19,12 @@ const Career = async () => {
     data.frontmatter;
   const callToAction = getListPage("sections/call-to-action.md");
 
-  // const jobPositions: JobPosition[] = await getJobPositions();
+  const jobPositions: JobPosition[] = await getJobPositions();
   // console.log("Fetched job positions: ", jobPositions);
-  const jobPositions: JobPosition[] = await sanityFetch({
-    query: `*[_type == "jobPosition"]`,
-    tags: ["jobPosition"],
-  });
+  // const jobPositions: JobPosition[] = await sanityFetch({
+  //   query: `*[_type == "jobPosition"]`,
+  //   tags: ["jobPosition"],
+  // });
 
   return (
     <>
