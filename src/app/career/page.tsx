88db400e76c57b5eee8f678a3d99432fb0fda9f4
Ceiara Madrigal <ceiaram@ninthavenuefoods.com>
2024-07-23 08:51:21 -0700

    Test adding back .md file

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -21,9 +21,9 @@ const Career = async () => {
   const jobPositions: JobPosition[] = [];
   // console.log("Fetched job positions: ", jobPositions);
 
-  // const { title, meta_title, description, career_title, career_content, link } =
-  //   data.frontmatter;
-  // const callToAction = getListPage("sections/call-to-action.md");
+  const { title, meta_title, description, career_title, career_content, link } =
+    data.frontmatter;
+  const callToAction = getListPage("sections/call-to-action.md");
 
   // const jobPositions: JobPosition[] = await sanityFetch({
   //   query: `*[_type == "jobPosition"]`,
@@ -52,21 +52,21 @@ const Career = async () => {
             <div className="lg:col-11">
               <div className="row">
                 <div className="relative">
-                  {/* <h2
+                  <h2
                     className="text-dark-grey pb-6 text-h3 lg:text-h2 animate-fade animate-duration-[600ms] ease-in"
                     dangerouslySetInnerHTML={markdownify(career_title)}
-                  /> */}
+                  />
                 </div>
               </div>
               <JobPositionCard jobPositions={jobPositions}></JobPositionCard>
-              {/* <p
+              <p
                 className="text-lg animate-fade animate-delay-[200ms] ease-in"
                 dangerouslySetInnerHTML={markdownify(career_content)}
-              /> */}
+              />
             </div>
           </div>
         </div>
-        {/* <CallToAction data={callToAction}></CallToAction> */}
+        <CallToAction data={callToAction}></CallToAction>
       </section>
     </>
   );
